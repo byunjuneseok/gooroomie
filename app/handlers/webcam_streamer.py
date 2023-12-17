f183d78b1ae3cacc6c9cf7a3df44ebc7c6644022
byunjuneseok <byunjuneseok@gmail.com>
2023-12-17 19:14:04 +0900

    chore: Temporarily disable audio for linux

--- app/handlers/webcam_streamer.py
+++ app/handlers/webcam_streamer.py
@@ -19,7 +19,7 @@ class WebcamStreamer:
                 video_player = MediaPlayer("video=Integrated Camera", format="dshow", options=video_options)
             case _:
                 video_player = MediaPlayer("/dev/video0", format="v4l2", options=video_options)
-                audio_player = MediaPlayer("default", format="alsa")
+                # audio_player = MediaPlayer("default", format="alsa")
         return (
             self.audio_media_relay.subscribe(audio_player.audio) if audio_player else None,
             self.video_media_relay.subscribe(video_player.video) if video_player else None,
